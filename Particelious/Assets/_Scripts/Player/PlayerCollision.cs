b16b8f6453b99cbae9a2aaeb349d2557faf76d43
Zcience <david.liebemann@gmail.com>
2017-08-12 12:10:27 +0200

    Adjusted Touch Slider

---
+++ Particelious/Assets/_Scripts/Player/PlayerCollision.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class PlayerCollision : MonoBehaviour {
+
+    private int HitCounter = 0; // Just for debugging
+
+	// Use this for initialization
+	void Start () {
+	}
+
+    private void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            HitCounter++;
+            Destroy(collision.gameObject);
+        }
+    }
+}
