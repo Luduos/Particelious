4fd5cfb4630d51c5d3c3d6051c511490944c7867
luduos <david.liebemann@gmail.com>
2017-10-12 23:00:25 +0200

    Dummy Levels and created a script?

---
+++ Particelious/Assets/_Scripts/UI/LevelSelection.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelection : MonoBehaviour {
+    [SerializeField]
+    private Button NextPageButton;
+    [SerializeField]
+    private Button PreviousPageButton;
+    [SerializeField]
+    private Text PageNumberText;
+    [SerializeField]
+    private Button[] LevelSelectionButtons;
+
+    [SerializeField]
+    private int NumberOfLevels;
+
+    private int CurrentPage = 1;
+    private int NumberOfPages = 1;
+
+    private void Start()
+    {
+        
+    }
+    
+}
